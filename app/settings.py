193c74d7d3374875a3c5fda36ff75a2d9b96982e
jfoj-dev <jfoj365@gmail.com>
2025-05-11 21:06:29 -0300

    Criação do Form User Register

--- app/settings.py
+++ app/settings.py
@@ -36,9 +36,10 @@ INSTALLED_APPS = [
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
-    'django.contrib.messages',
+    'django.contrib.messages', 
     'django.contrib.staticfiles',
     'cars',
+    'accounts',
 ]
 
 MIDDLEWARE = [
