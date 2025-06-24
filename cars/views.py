65e4981d160b0ad34b0f16d6cf77025d4a2f426d
jfoj-dev <jfoj365@gmail.com>
2025-06-24 06:37:15 -0300

    Reescrevendo new_car_view(View Base)

--- cars/views.py
+++ cars/views.py
@@ -3,22 +3,6 @@ from cars.models import Car
 from cars.forms import CarModelForm
 from django.views import View
  
-
-# Create your views here.
-
-def cars_view(request):
-   cars = Car.objects.all().order_by('model')
-   search = request.GET.get('search')
-
-   if search:
-      cars = cars.filter(model__icontains=search)
-   
-   return render(
-       request, 
-       'cars.html',
-       {'cars': cars}
-    )
-
 class CarsView(View):
 
    def get(self, request):
@@ -32,14 +16,41 @@ class CarsView(View):
          request, 
          'cars.html',
          {'cars': cars}
-      )   
+      ) 
 
-def new_car_view(request):
-    if request.method == 'POST':
-          new_car_form = CarModelForm(request.POST, request.FILES)
-          if new_car_form.is_valid():
-              new_car_form.save()
-              return redirect('cars_list')
-    else:    
-       new_car_form = CarModelForm()
-    return render(request, 'new_car.html', {'new_car_form': new_car_form})
+class NewCarView(View):
+   def get(self, request):
+      new_car_form = CarModelForm()
+      return render(request, 'new_car.html', {'new_car_form': new_car_form})
+   
+   def post(self, request):
+      new_car_form = CarModelForm(request.POST, request.FILES)
+      if new_car_form.is_valid():
+         new_car_form.save()
+         return redirect('cars_list')
+      return render(request, 'cars.html', {'cars': cars})
+
+#def new_car_view(request):
+#    if request.method == 'POST':
+#          new_car_form = CarModelForm(request.POST, request.FILES)
+#          if new_car_form.is_valid():
+#              new_car_form.save()
+#              return redirect('cars_list')
+#    else:    
+#       new_car_form = CarModelForm()
+#    return render(request, 'new_car.html', {'new_car_form': new_car_form})   
+
+# Create your views here.
+
+#def cars_view(request):
+#   cars = Car.objects.all().order_by('model')
+#   search = request.GET.get('search')
+
+#   if search:
+#      cars = cars.filter(model__icontains=search)
+   
+#   return render(
+#       request, 
+#       'cars.html',
+#       {'cars': cars}
+#    )
