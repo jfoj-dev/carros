4c6803308c72c25354532eafcca9af2c1947a202
jfoj-dev <jfoj365@gmail.com>
2025-07-15 21:36:50 -0300

    config allowed_hosts

--- app/settings.py
+++ app/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-3*-eu^d!=b%y^zhdil(u9xjwy0#e@0yzntvxfu+3&6sg+$1m_+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
